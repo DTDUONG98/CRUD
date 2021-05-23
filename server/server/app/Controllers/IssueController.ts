7b1d74d38f53e2c64680bad83748f78de9032092
huydv <vanhuy2612@gmail.com>
2021-05-23 18:55:21 +0700

    fix api get issues by projectId

--- server/server/app/Controllers/IssueController.ts
+++ server/server/app/Controllers/IssueController.ts
@@ -1,11 +1,14 @@
 import BaseController from './BaseController';
 import IssueModel from '@app/Models/IssueModel';
+import ProjectModel from '@app/Models/ProjectModel';
+
 import _ from 'lodash';
 import ApiException from '../Exceptions/ApiException';
 
 export default class IssueController extends BaseController {
 
     Model = IssueModel;
+    ProjectModel = ProjectModel;
 
     async index() {
 
@@ -24,6 +27,9 @@ export default class IssueController extends BaseController {
         let params = this.validate(inputs, allowFields, {removeNotAllows: false});
         const projectId = params['projectId'];
         if (!projectId) throw new ApiException(9996, "projectID is required!");
+        
+        const project = await this.ProjectModel.getById(projectId);
+        if (!project) throw new ApiException(7002, 'Project is not exist');
         
         let data = await this.Model.query()
             .where("projectId", projectId)
